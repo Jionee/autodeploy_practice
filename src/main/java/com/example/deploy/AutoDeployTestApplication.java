fe04ceffb9813108f6f879d5cf159d0008651f3e
jiwon_ryu <happyjw3910@gmail.com>
2021-12-24 10:41:00 +0900

    [INIT]

---
+++ src/main/java/com/example/deploy/AutoDeployTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.deploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoDeployTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoDeployTestApplication.class, args);
+	}
+
+}
